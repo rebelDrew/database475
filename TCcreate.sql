7074dd3ec65577f20346a76c498ce18716b0406f
David Rodriguez <drodrig1@go.olemiss.edu>
2019-03-28 18:39:15 -0500

    change primary key constraint

--- TCcreate.sql
+++ TCcreate.sql
@@ -11,80 +11,72 @@ DROP TABLE IF EXISTS payment;
 -- tables
 -- Table: ClockIn
 CREATE TABLE ClockIn (
-    in_id int NOT NULL,
+    in_id int NOT NULL PRIMARY KEY,
     date date NOT NULL,
     in_time time NOT NULL,
-    Staff_staff_id int NOT NULL,
-    CONSTRAINT ClockIn_pk PRIMARY KEY (in_id)
+    Staff_staff_id int NOT NULL
 );
 
 -- Table: ClockOut
 CREATE TABLE ClockOut (
-    out_id int NOT NULL,
+    out_id int NOT NULL PRIMARY KEY,
     ClockIn_in_id int NOT NULL,
-    out_time time NOT NULL,
-    CONSTRAINT ClockOut_pk PRIMARY KEY (out_id)
+    out_time time NOT NULL
 );
 
 -- Table: Departments
 CREATE TABLE Departments (
-    depart_id int NOT NULL,
+    depart_id int NOT NULL PRIMARY KEY,
     name varchar(15) NOT NULL,
     type varchar(15) NOT NULL,
-    email varchar(10) NOT NULL,
-    CONSTRAINT Departments_pk PRIMARY KEY (depart_id)
+    email varchar(10) NOT NULL
 );
 
 -- Table: Members
 CREATE TABLE Members (
-    mem_id int NOT NULL,
+    mem_id int NOT NULL PRIMARY KEY,
     FName int NOT NULL,
     LName int NOT NULL,
     type varchar(15) NOT NULL,
     cost double(8,2) NOT NULL,
     DOB date NOT NULL,
-    email varchar(20) NOT NULL,
-    CONSTRAINT Members_pk PRIMARY KEY (mem_id)
+    email varchar(20) NOT NULL
 );
 
 -- Table: Pro_members
 CREATE TABLE Pro_members (
-    Members_mem_id int NOT NULL,
+    Members_mem_id int NOT NULL PRIMARY KEY,
     Programs_pro_id int NOT NULL,
-    experienceLevel varchar(10) NOT NULL,
-    CONSTRAINT Pro_members_pk PRIMARY KEY (Members_mem_id,Programs_pro_id)
+    experienceLevel varchar(10) NOT NULL
 );
 
 -- Table: Programs
 CREATE TABLE Programs (
-    pro_id int NOT NULL,
+    pro_id int NOT NULL PRIMARY KEY,
     type varchar(15) NOT NULL,
-    Departments_depart_id int NOT NULL,
-    CONSTRAINT Programs_pk PRIMARY KEY (pro_id)
+    Departments_depart_id int NOT NULL
 );
 
 -- Table: Staff
 CREATE TABLE Staff (
-    staff_id int NOT NULL,
+    staff_id int NOT NULL PRIMARY KEY,
     FName varchar(15) NOT NULL,
     LName varchar(15) NOT NULL,
     type varchar(15) NOT NULL,
     email varchar(20) NOT NULL,
     phone varchar(10) NOT NULL,
-    Departments_depart_id int NOT NULL,
-    CONSTRAINT Staff_pk PRIMARY KEY (staff_id)
+    Departments_depart_id int NOT NULL
 );
 
 -- Table: payment
 CREATE TABLE payment (
-    pay_id int NOT NULL,
+    pay_id int NOT NULL PRIMARY KEY,
     pay_type varchar(10) NOT NULL,
     rate double(8,2) NOT NULL,
-    Staff_staff_id int NOT NULL,
-    CONSTRAINT payment_pk PRIMARY KEY (pay_id)
+    Staff_staff_id int NOT NULL
 );
 
--- foreign keys
+-- -- foreign keys
 -- Reference: ClockIn_Staff (table: ClockIn)
 ALTER TABLE ClockIn ADD CONSTRAINT ClockIn_Staff FOREIGN KEY ClockIn_Staff (Staff_staff_id)
     REFERENCES Staff (staff_id);
@@ -112,5 +104,5 @@ ALTER TABLE Staff ADD CONSTRAINT Staff_Departments FOREIGN KEY Staff_Departments
 -- Reference: payment_Staff (table: payment)
 ALTER TABLE payment ADD CONSTRAINT payment_Staff FOREIGN KEY payment_Staff (Staff_staff_id)
     REFERENCES Staff (staff_id);
-
--- End of file.
+--
+-- -- End of file.
