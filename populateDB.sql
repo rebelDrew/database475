b3a3776dd75ab417b9737f95de1b91518a4092d6
xxbamaxbeastxx <xxbamaxbeastxx@gmail.com>
2019-03-28 19:24:39 -0500

    changes to errors

--- populateDB.sql
+++ populateDB.sql
@@ -5,7 +5,7 @@ INSERT INTO Departments(name, type, email)
 values ('Aquatics', 'swimming', 'aquatics@go.olemiss.edu')
 
 INSERT INTO Departments(name, type, email)
-values ('Professional Staff', 'Directors', 'prostaff@go.olemiss.edu')
+values ('ProfessionalStaff', 'directors', 'prostaff@go.olemiss.edu')
 
 INSERT INTO Staff(FName, LName, type, email, phone, Departments_depart_id)
 values ('Andrew', 'Wallace', 'trainer', 'anwalla3@go.olemiss.edu', '5803019511', 1);
@@ -29,13 +29,13 @@ INSERT INTO Programs(type, Departments_depart_id)
 values ('Swim Class', 2);
 
 INSERT INTO Members(FName, LName, type, cost, DOB, email)
-values ('Joshua', 'Williamson', 'student', 0.00, 'joshwil32@go.olemiss.edu');
-
-INSERT INTO Members(FaAme, LName, type, cost, DOB, email)
-values ('John', 'Doe', 'student', 0.00, 'joedoe1@go.olemiss.edu');
+values ('Joshua', 'Williamson', 'student', 0.00, 11/21/1992, 'joshwil32@go.olemiss.edu');
 
 INSERT INTO Members(FName, LName, type, cost, DOB, email)
-values ('Yixin', 'Chen', 'faculty', 50.00, 'yixchen@go.olemiss.edu');
+values ('John', 'Doe', 'student', 0.00, 01/31/1998, 'joedoe1@go.olemiss.edu');
+
+INSERT INTO Members(FName, LName, type, cost, DOB, email)
+values ('Yixin', 'Chen', 'faculty', 50.00, 10/31/1982, 'yixchen@go.olemiss.edu');
 
 INSERT INTO Pro_members(Members_mem_id, Programs_pro_id, experienceLevel)
 values (1, 1, 'beginner');
