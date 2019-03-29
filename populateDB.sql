aed74004398d976b65a19c7302bd3a806b753727
xxbamaxbeastxx <xxbamaxbeastxx@gmail.com>
2019-03-28 19:19:59 -0500

    cool

--- populateDB.sql
+++ populateDB.sql
@@ -5,7 +5,7 @@ INSERT INTO Departments(name, type, email)
 values ('Aquatics', 'swimming', 'aquatics@go.olemiss.edu')
 
 INSERT INTO Departments(name, type, email)
-values ('Professional Staff', 'GAs/Directors', 'prostaff@go.olemiss.edu')
+values ('Professional Staff', 'Directors', 'prostaff@go.olemiss.edu')
 
 INSERT INTO Staff(FName, LName, type, email, phone, Departments_depart_id)
 values ('Andrew', 'Wallace', 'trainer', 'anwalla3@go.olemiss.edu', '5803019511', 1);
@@ -13,10 +13,10 @@ values ('Andrew', 'Wallace', 'trainer', 'anwalla3@go.olemiss.edu', '5803019511',
 INSERT INTO Staff(FName, LName, type, email, phone, Departments_depart_id)
 values ('Austin', 'Wallace', 'trainer', 'amwalla3@go.olemiss.edu', '5803019401', 1);
 
-INSERT INTO Staff(staff_id, FName, LName, type, email, phone, Departments_depart_id)
+INSERT INTO Staff(FName, LName, type, email, phone, Departments_depart_id)
 values ('Joey', 'Ele', 'lifeguard', 'joeEle4@go.olemiss.edu', '90139281111', 2);
 
-INSERT INTO Staff(staff_id, FName, LName, type, email, phone, Departments_depart_id)
+INSERT INTO Staff(FName, LName, type, email, phone, Departments_depart_id)
 values ('Jeff', 'Lucas', 'Directory', 'jeffLuc2@go.olemiss.edu', '6623918821', 3);
 
 INSERT INTO Programs(type, Departments_depart_id)
