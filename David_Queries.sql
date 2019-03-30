b6becc182d69cb287e5203e550cfb69b4092d7ce
David Rodriguez <drodrig1@go.olemiss.edu>
2019-03-29 19:01:14 -0500

    a

--- David_Queries.sql
+++ David_Queries.sql
@@ -1,10 +1,11 @@
+-- display the payment rates for staff who work at department 1
 select payment.rate
 from payment
 where payment.Staff_staff_id in
 (select Staff.staff_id from Staff where Staff.Departments_depart_id = 1);
 
 
-
+-- for each program type, list all of the members first and last name
 select group_concat(FName, " ", LName) as Name,
 program_type as Program
 from Programs left outer join Pro_members
